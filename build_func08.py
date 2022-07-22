3d6eb5d55f065765e511f9bb1e9b42c83efb76b5
Mironshoh <mironshoh1545@gmail.com>
2022-07-22 11:10:52 +0500

    .\build_func03 - build_func10

--- build_func08.py
+++ build_func08.py
@@ -1,2 +1,8 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func08
+
+
+x = 7
+y = 1
+answer = 5 * x ** 2 + x * y ** 2
+print(answer)
