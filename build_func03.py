4e6c425da19e08de86843ca9d0a6c149930f308d
Mironshoh <mironshoh1545@gmail.com>
2022-07-22 10:42:13 +0500

    .\build_func03.py

--- build_func03.py
+++ build_func03.py
@@ -1,7 +1,11 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
 
+from multiprocessing.connection import answer_challenge
+
+
 def build_func03(n):
-    return 3 * (n + 1) ** 2
+    answer = 3 * (n + 1) ** 2
+    return answer
 n = 3.5
 print(build_func03(n))
